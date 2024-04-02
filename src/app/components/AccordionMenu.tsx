2b4554d9c8439234199eb5d0661b9e5b2df8c84d
Rick Vermeil <rickv85@gmail.com>
2024-04-02 17:03:00 -0600

    Create AccordionMenu from Menu components

---
+++ src/app/components/AccordionMenu.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { useState } from "react";
+import { IconButton, Menu, MenuItem } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
+export default function AccordionMenu() {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <IconButton
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        sx={{ mr: 2 }}
+      >
+        <MenuIcon />
+      </IconButton>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem onClick={handleClose}>Chat</MenuItem>
+        <MenuItem onClick={handleClose}>Conversation History</MenuItem>
+      </Menu>
+    </div>
+  );
+}
--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,21 +1,13 @@
-import { AppBar, Box, Toolbar, Typography, IconButton, MenuList } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import AccordionMenu from "./AccordionMenu";
 
 export default function Nav() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <AccordionMenu />
+          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
             CodeAide
           </Typography>
         </Toolbar>
