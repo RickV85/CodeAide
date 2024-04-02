2108a2eaa139fc40022b6f2717b563a83bdae086
Rick Vermeil <rickv85@gmail.com>
2024-04-02 16:47:40 -0600

    Create Nav from AppBar component

---
+++ src/app/components/Nav.tsx
@@ -0,0 +1,25 @@
+import { AppBar, Box, Toolbar, Typography, IconButton, MenuList } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
+export default function Nav() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            CodeAide
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
