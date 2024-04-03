01976c38dfc5d79c72c2a1e839baa0aefc210355
Rick Vermeil <rickv85@gmail.com>
2024-04-02 18:38:46 -0600

    Create conditional rendering for AccordionMenu

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,12 +1,23 @@
-import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+"use client";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
 import AccordionMenu from "./AccordionMenu";
 
 export default function Nav() {
+  const theme = useTheme();
+  const isMobileDisplay = useMediaQuery(theme.breakpoints.between("xs", "lg"));
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <AccordionMenu />
+          {isMobileDisplay && <AccordionMenu />}
           <Typography variant="h5" component="h1" sx={{ flexGrow: 1 }}>
             CodeAide
           </Typography>
