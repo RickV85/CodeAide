24a56e88ea325defc2ec677aeb7a0622f4fd83d8
Rick Vermeil <rickv85@gmail.com>
2024-04-04 12:03:42 -0600

    Create test for max char count

--- src/app/test/ChatTextInput.test.ts
+++ src/app/test/ChatTextInput.test.ts
@@ -6,4 +6,10 @@ describe("handleInput", () => {
     const input = "";
     expect(validateUserInput(input)).toBe("Input cannot be empty");
   });
+
+  test("it should not allow inputs longer than 1000 chars", () => {
+    const tooLong = "a".repeat(1001);
+
+    expect(validateUserInput(tooLong)).toBe("Max character count exceeded");
+  });
 });
