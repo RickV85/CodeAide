6c668c9fd74e337636dc1c5a4b608260d478e9f9
Rick Vermeil <rickv85@gmail.com>
2024-04-04 12:05:18 -0600

    Update validateUserInput to test input length

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -1,6 +1,9 @@
 export const validateUserInput = (input: string) => {
   if (input === "") {
     return "Input cannot be empty";
+  } else if (input.length > 1000) {
+    return "Max character count exceeded";
+  } else {
+    return "valid";
   }
-  return "valid";
 };
