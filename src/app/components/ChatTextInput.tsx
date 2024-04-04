cf19d0a761c815f124070cc02a4a8f0c08adc248
Rick Vermeil <rickv85@gmail.com>
2024-04-04 10:54:30 -0600

    Create userChatInput state, handleInput in ChatTextInput

--- src/app/components/ChatTextInput.tsx
+++ src/app/components/ChatTextInput.tsx
@@ -1,10 +1,18 @@
 "use client";
 import { TextField, Box } from "@mui/material";
+import { useContext } from "react";
+import { AppContext } from "../contexts/AppContext";
 export default function ChatTextInput() {
+  const { userChatInput, setUserChatInput } = useContext(AppContext);
+
+  const handleInput = (input: string) => {
+    setUserChatInput(input);
+  };
+
   return (
     <Box
       sx={{
-        padding: "1rem"
+        padding: "1rem",
       }}
     >
       <TextField
@@ -14,6 +22,8 @@ export default function ChatTextInput() {
         placeholder="Enter question or response here"
         variant="outlined"
         fullWidth
+        value={userChatInput}
+        onChange={(e) => handleInput(e.target.value)}
       />
     </Box>
   );
--- src/app/contexts/AppContext.tsx
+++ src/app/contexts/AppContext.tsx
@@ -4,11 +4,15 @@ import React, { createContext, useState } from "react";
 interface AppContext {
   activeChat: string;
   setActiveChat: React.Dispatch<React.SetStateAction<string>>;
+  userChatInput: string;
+  setUserChatInput: React.Dispatch<React.SetStateAction<string>>;
 }
 
 export const AppContext = createContext<AppContext>({
   activeChat: "",
   setActiveChat: () => {},
+  userChatInput: "",
+  setUserChatInput: () => {},
 });
 
 interface AppProviderProps {
@@ -17,8 +21,11 @@ interface AppProviderProps {
 
 export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
   const [activeChat, setActiveChat] = useState("");
+  const [userChatInput, setUserChatInput] = useState("");
   return (
-    <AppContext.Provider value={{ activeChat, setActiveChat }}>
+    <AppContext.Provider
+      value={{ activeChat, setActiveChat, userChatInput, setUserChatInput }}
+    >
       {children}
     </AppContext.Provider>
   );
