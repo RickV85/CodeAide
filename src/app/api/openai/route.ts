c611da4b1d6732a7bd5d82fee0490fcea55d7993
Rick Vermeil <rickv85@gmail.com>
2024-04-05 09:10:19 -0600

    Create initial system prompt, settings and messages structure

--- src/app/api/openai/route.ts
+++ src/app/api/openai/route.ts
@@ -10,12 +10,24 @@ export const runtime = "edge";
 
 export async function POST(req: Request) {
   try {
-    const { messages } = await req.json();
+    const { messageHistory } = await req.json();
 
     const response = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
       stream: true,
-      messages,
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are an assistant for software developers that will not return code to the user. You are designed to assist the user in the same way that a mentor or teacher would, giving the user helpful tips and direction, but not solving the problem or issue for them. Provide cited sources for your answers and links to the resources the information came from.",
+        },
+        ...messageHistory,
+      ],
+      temperature: 1,
+      max_tokens: 1000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0.5,
     });
 
     const stream = OpenAIStream(response);
