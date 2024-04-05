28abf2504b58d2d363d93da9be7550f117d63a8f
Rick Vermeil <rickv85@gmail.com>
2024-04-05 14:40:31 -0600

    Create intial Conversation disaply by keys

--- src/app/components/Conversations.tsx
+++ src/app/components/Conversations.tsx
@@ -1,8 +1,40 @@
 "use client";
 import { Box, Divider, Typography, useTheme } from "@mui/material";
+import { useContext, useEffect, useState } from "react";
+import { AppContext } from "../contexts/AppContext";
 
 export default function Conversations() {
+  const { activeChat } = useContext(AppContext);
+  const [conversations, setConversations] = useState<any>([]);
   const theme = useTheme();
+
+  const getHistory = () => {
+    const history = window.localStorage.getItem("conversations");
+    if (history) {
+      const parsedHist = JSON.parse(history);
+      const convKeys = Object.keys(parsedHist);
+      if (convKeys.length) {
+        return convKeys;
+      }
+    }
+  };
+
+  useEffect(() => {
+    const updatedConversations = getHistory();
+    if (updatedConversations?.length) {
+      setConversations(updatedConversations);
+    }
+  }, [activeChat, conversations]);
+
+  const renderConversations = () => {
+    const convElements = conversations.map((key: any) => {
+      return <Box key={`conv-${key}`}>
+        <Typography>{key}</Typography>
+      </Box>
+    })
+    return convElements;
+  }
+
   return (
     <Box
       sx={{
@@ -15,8 +47,11 @@ export default function Conversations() {
         borderRadius: "4px",
       }}
     >
-      <Typography variant="h6" textAlign={"center"}>Conversations</Typography>
+      <Typography variant="h6" textAlign={"center"}>
+        Conversations
+      </Typography>
       <Divider variant="middle" />
+      {renderConversations()}
     </Box>
   );
 }
