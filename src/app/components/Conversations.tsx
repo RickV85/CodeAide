603d7875f4cda1eaad49ebeb3e6303ac7866becc
Rick Vermeil <rickv85@gmail.com>
2024-04-06 15:46:45 -0600

    Add delete icon and style to Conversations

--- src/app/components/Conversations.tsx
+++ src/app/components/Conversations.tsx
@@ -1,9 +1,10 @@
 "use client";
-import { Box, Divider, Typography, useTheme } from "@mui/material";
+import { Box, Divider, Grid, Typography, useTheme } from "@mui/material";
 import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../contexts/AppContext";
 import { ConversationRepo } from "../classes/ConversationRepo";
 import { Conversation } from "../classes/Conversation";
+import CloseIcon from "@mui/icons-material/Close";
 
 export default function Conversations() {
   const { activeChat, setActiveChat, setMessages } = useContext(AppContext);
@@ -45,15 +46,23 @@ export default function Conversations() {
               key={`conv-${conv.id}`}
               marginTop="1rem"
               onClick={() => makeConvActive(conv.id)}
-              title="Click to make active conversation"
+              title="Make active conversation"
               sx={{
                 border: `1px solid ${theme.palette.primary.light}`,
                 borderRadius: "4px",
-                padding: "0.5rem",
+                padding: "0.25rem",
                 cursor: "pointer",
+                display: "flex",
+                justifyContent: "space-between",
               }}
             >
               <Typography>{conv.createIntro()}</Typography>
+              <Box
+                title="Delete conversation"
+                sx={{ marginLeft: "0.5rem" }}
+              >
+                <CloseIcon />
+              </Box>
             </Box>
           );
         }
