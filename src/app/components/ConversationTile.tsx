fccd70935ae9795408cd4592715d18509e6245ef
Rick Vermeil <rickv85@gmail.com>
2024-04-06 15:55:45 -0600

    Create ConversationTile to render in Conversations

---
+++ src/app/components/ConversationTile.tsx
@@ -0,0 +1,45 @@
+import { Box, Typography } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import { useTheme } from "@mui/material";
+import { Conversation } from "../classes/Conversation";
+import { useContext } from "react";
+import { AppContext } from "../contexts/AppContext";
+
+interface Props {
+  conversation: Conversation;
+}
+
+export default function ConversationTile({conversation}: Props) {
+  const {setActiveChat, setMessages} = useContext(AppContext);
+  const theme = useTheme();
+
+  const makeConvActive = (id: string | null) => {
+    if (id && conversationRepo) {
+      const foundConv = conversationRepo.findConvById(id);
+      if (foundConv) {
+        setActiveChat(foundConv.messages);
+        setMessages(foundConv.messages);
+      }
+    }
+  };
+  return (
+    <Box
+      marginTop="1rem"
+      onClick={() => makeConvActive(conversation.id)}
+      title="Make active conversation"
+      sx={{
+        border: `1px solid ${theme.palette.primary.light}`,
+        borderRadius: "4px",
+        padding: "0.25rem",
+        cursor: "pointer",
+        display: "flex",
+        justifyContent: "space-between",
+      }}
+    >
+      <Typography>{conversation.createIntro()}</Typography>
+      <Box title="Delete conversation" sx={{ marginLeft: "0.5rem" }}>
+        <CloseIcon />
+      </Box>
+    </Box>
+  );
+}
--- src/app/components/Conversations.tsx
+++ src/app/components/Conversations.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { Box, Divider, Grid, Typography, useTheme } from "@mui/material";
+import { Box, Divider, Typography, useTheme } from "@mui/material";
 import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../contexts/AppContext";
 import { ConversationRepo } from "../classes/ConversationRepo";
 import { Conversation } from "../classes/Conversation";
-import CloseIcon from "@mui/icons-material/Close";
+import ConversationTile from "./ConversationTile";
 
 export default function Conversations() {
   const { activeChat, setActiveChat, setMessages } = useContext(AppContext);
@@ -27,43 +27,34 @@ export default function Conversations() {
     }
   }, [activeChat]);
 
-  const makeConvActive = (id: string | null) => {
-    if (id && conversationRepo) {
-      const foundConv = conversationRepo.findConvById(id);
-      if (foundConv) {
-        setActiveChat(foundConv.messages);
-        setMessages(foundConv.messages);
-      }
-    }
-  };
-
   const renderConversations = () => {
     if (conversationRepo?.conversations.length) {
       const convElements = conversationRepo?.conversations.map(
         (conv: Conversation) => {
           return (
-            <Box
-              key={`conv-${conv.id}`}
-              marginTop="1rem"
-              onClick={() => makeConvActive(conv.id)}
-              title="Make active conversation"
-              sx={{
-                border: `1px solid ${theme.palette.primary.light}`,
-                borderRadius: "4px",
-                padding: "0.25rem",
-                cursor: "pointer",
-                display: "flex",
-                justifyContent: "space-between",
-              }}
-            >
-              <Typography>{conv.createIntro()}</Typography>
-              <Box
-                title="Delete conversation"
-                sx={{ marginLeft: "0.5rem" }}
-              >
-                <CloseIcon />
-              </Box>
-            </Box>
+            <ConversationTile key={`conv-${conv.id}`} conversation={conv} />
+            // <Box
+            //   key={`conv-${conv.id}`}
+            //   marginTop="1rem"
+            //   onClick={() => makeConvActive(conv.id)}
+            //   title="Make active conversation"
+            //   sx={{
+            //     border: `1px solid ${theme.palette.primary.light}`,
+            //     borderRadius: "4px",
+            //     padding: "0.25rem",
+            //     cursor: "pointer",
+            //     display: "flex",
+            //     justifyContent: "space-between",
+            //   }}
+            // >
+            //   <Typography>{conv.createIntro()}</Typography>
+            //   <Box
+            //     title="Delete conversation"
+            //     sx={{ marginLeft: "0.5rem" }}
+            //   >
+            //     <CloseIcon />
+            //   </Box>
+            // </Box>
           );
         }
       );
