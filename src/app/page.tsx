6bd54a0a8a615fe964d5d435efa17d0e67781636
Rick Vermeil <rickv85@gmail.com>
2024-04-07 11:25:50 -0600

    Add conditional rendering on Home of Conversations or Chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,32 @@
+"use client";
 import styles from "./page.module.css";
 import Nav from "./components/Nav";
 import { Grid } from "@mui/material";
 import Chat from "./components/Chat";
 import Conversations from "./components/Conversations";
+import { useContext } from "react";
+import { AppContext } from "./contexts/AppContext";
 
 export default function Home() {
+  const { showConvView } = useContext(AppContext);
   return (
     <main className={styles.main}>
       <Nav />
       <Grid container spacing={0}>
         <Grid
           item
-          xs={false}
+          xs={showConvView ? 12 : false}
           lg={4}
-          sx={{ display: { xs: "none", lg: "block" } }}
+          sx={{ display: { xs: showConvView ? "block" : "none", lg: "block" } }}
         >
           <Conversations />
         </Grid>
-        <Grid item xs={12} lg={8}>
+        <Grid
+          item
+          xs={showConvView ? false : 12}
+          lg={8}
+          sx={{ display: { xs: showConvView ? "none" : "block", lg: "block" } }}
+        >
           <Chat />
         </Grid>
       </Grid>
